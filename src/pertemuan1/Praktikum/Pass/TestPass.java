62ad4e1ba71b00902be5712f350e15559d865797
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2020-10-20 11:53:33 +0700

    modul 1

---
+++ src/pertemuan1/Praktikum/Pass/TestPass.java
@@ -0,0 +1,20 @@
+package pertemuan1.Praktikum.Pass;
+
+public class TestPass {
+    int nomer1,nomer2;
+
+    public TestPass(int nomer1, int nomer2) {
+        this.nomer1 = nomer1;
+        this.nomer2 = nomer2;
+    }
+
+    void calculate(int m,int n){
+        m = m*10;
+        n = n/2;
+    }
+
+    void calculate(TestPass passed){
+        passed.nomer1 = passed.nomer1 * 10;
+        passed.nomer2 = passed.nomer2 / 2;
+    }
+}
