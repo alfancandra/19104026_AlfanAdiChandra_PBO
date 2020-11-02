5bc8f7b0fed9060eaaa3d407911d27e84551808e
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2020-11-02 19:03:13 +0700

    Praktikum Pertemuan 3

--- Praktikum_PBO/src/pertemuan4/Tugas/no1/Main.java
+++ Praktikum_PBO/src/pertemuan4/Tugas/no1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Deklarasi
         Employee employee = new SalariedEmployee("Boruto", 44, 400000);
-        Employee employee1 = new CommisionEmployee("Naruto", 22, 5000000, 55, 250);
+        Employee employee1 = new CommisionEmployee("Naruto", 22, 5000000, 50000, 250);
         Employee employee2 = new ProjectPlanner("Kirito", 55, 2500000, 20000, 80);
 
         // Cetak
